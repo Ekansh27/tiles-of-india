e254f614f805064c3c6659476f5c167a9c2b8e39
Ekansh Arora <aroraekansh2706@gmail.com>
2025-12-27 22:02:36 -0800

    fixing zoom mistake on phone

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Tiles of India",
   description: "Master Indian words for competitive Scrabble",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 }
 
 export default function RootLayout({
